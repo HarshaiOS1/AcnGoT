edd373e01c30c719f02a926fb88f9a2c3b3eb58c
HarshaiOS1 <kharsha21@yahoo.in>
2022-09-11 13:38:40 +0200

    Added pods, lottie animation pod and created folder for clean architecure

---
+++ ACNGoT/Pods/Target Support Files/Pods-ACNGoT-ACNGoTUITests/Pods-ACNGoT-ACNGoTUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ACNGoT_ACNGoTUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ACNGoT_ACNGoTUITests
+@end
---
+++ ACNGoT/Pods/Target Support Files/Pods-ACNGoT/Pods-ACNGoT-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ACNGoT : NSObject
+@end
+@implementation PodsDummy_Pods_ACNGoT
+@end
